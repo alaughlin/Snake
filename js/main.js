b129a631708943b6dfbb55e13665a3f3a7a32bbf
alaughlin <adamlaughlin@outlook.com>
2014-09-20 02:03:33 -0400

    refactoring of code.

--- js/main.js
+++ js/main.js
@@ -1,29 +1,41 @@
 $(document).ready(function () {
   var game = window.game = {};
 
-  game.renderBoard = function () {
-    game.snakeHead = _.last(game.snake);
-    if (game.snakeHead[0] < 0 || game.snakeHead[0] > 19 || game.snakeHead[1] < 0 || game.snakeHead[1] > 19 ) {
-      game.initialize();
+  game.changeDirection = function (keyCode) {
+    switch(keyCode) {
+    case 37:
+      if (game.direction != 39) { game.direction = "left" }
+      break;
+    case 39:
+      if (game.direction != 37) { game.direction = "right" }
+      break;
+    case 38:
+      if (game.direction != 40) { game.direction = "up" }
+      break;
+    case 40:
+      if (game.direction != 38) { game.direction = "down" }
+      break;
     }
+  };
 
+  game.applyDeltas = function () {
     var xDelta;
     var yDelta;
     
     switch(game.direction) {
-    case 37:
+    case "left":
       xDelta = -1;
       yDelta = 0;
       break;
-    case 38:
+    case "up":
       xDelta = 0;
       yDelta = -1;
       break;
-    case 39:
+    case "right":
       xDelta = 1;
       yDelta = 0;
       break;
-    case 40:
+    case "down":
       xDelta = 0;
       yDelta = 1;
       break;
@@ -33,11 +45,27 @@ $(document).ready(function () {
       break;
     }
 
-    if (game.direction) {
-      game.snake.shift();
-      game.snake.push([_.last(game.snake)[0] + yDelta, _.last(game.snake)[1] + xDelta]);
+    return [xDelta, yDelta];
+  };
+
+  game.moveSnake = function () {
+    game.snake.shift();
+    game.snake.push([_.last(game.snake)[0] + game.applyDeltas()[1], _.last(game.snake)[1] + game.applyDeltas()[0]]);
+  };
+
+  game.renderBoard = function () {
+    game.snakeHead = _.last(game.snake);
+
+    // reset if out-of-bounds
+    if (game.snakeHead[0] < 0 || game.snakeHead[0] > 19 || game.snakeHead[1] < 0 || game.snakeHead[1] > 19 ) {
+      game.initialize();
     }
 
+    if (game.moving) {
+      game.moveSnake();
+    }
+
+    // wipe squares and redraw
     $('.square').removeClass('snake-spot');
 
     _.each(game.snake, function (spot) {
@@ -48,9 +76,12 @@ $(document).ready(function () {
   game.initialize = function () {
     game.board = [];
     game.snake = [[10, 10], [10, 11], [10, 12]];
-    game.direction = null;
+    game.moving = false;
+    game.direction = "right";
+    // we need to wipe the board when a reset happens
     $('#game-board').html("");
 
+    // make board array and draw it
     for (var i = 0; i < 20; i++) {
       game.board.push([]);
       for (var j = 0; j < 20; j++) {
@@ -60,13 +91,14 @@ $(document).ready(function () {
     }
 
     Mousetrap.bind(['left', 'up', 'right', 'down'], function (event) {
-      game.direction = event.keyCode;
+      game.moving = true;
+      game.changeDirection(event.keyCode);
       return false;
     });
   };
 
   game.initialize();
   window.setInterval(function () {
-      game.renderBoard();
-    }, 75);
+    game.renderBoard();
+  }, 75);
 });
